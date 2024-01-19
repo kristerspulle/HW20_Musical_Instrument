45d2c9ce614d65f8d9bc12cf811da4dd06d6315a
Kristers Pulle <pulle265@gmail.com>
2024-01-19 13:25:44 +0200

    update

--- src/components/PlayButton/PlayButton.tsx
+++ src/components/PlayButton/PlayButton.tsx
@@ -1,6 +1,24 @@
-import styles from './PlayButton.module.css'
+import styles from './PlayButton.module.css';
 type PlayButtonProps = {
-  soundFile: string,
-  onClick: (index) => void
-}
-export const PlayButton = ({soundFile, onClick}: PlayButtonProps) => <div className={styles.key} onClick={onClick}><audio src={soundFile}/></div>
+  soundFile: string;
+};
+
+export const PlayButton = ({ soundFile }: PlayButtonProps) => {
+  let isPlaying = false
+  const handleButtonClick = (soundFile: string) => {
+    
+    const audio = new Audio(soundFile);
+    if(isPlaying) {
+      audio.pause();
+    } else {
+      audio.play();
+    }
+    isPlaying = !isPlaying
+  };
+
+  return (
+    <div className={styles.key} onClick={() => handleButtonClick(soundFile)}>
+      <audio src={soundFile} />
+    </div>
+  );
+};
--- src/pages/MusicPage/MusicPage.tsx
+++ src/pages/MusicPage/MusicPage.tsx
@@ -5,51 +5,38 @@ import { PlayButton } from '../../components/PlayButton/PlayButton';
 import styles from './MusicPage.module.css';
 
 const sounds = [
-  'assets/bass1.wav',
-  'assets/bass2.wav',
-  'assets/fx1.wav',
-  'assets/fx2.wav',
-  'assets/melody1.wav',
-  'assets/melody2.wav',
-  'assets/melody3.wav',
-  'assets/melody4.wav',
-  'assets/melody5.wav',
-  'assets/melody6.wav',
-  'assets/percussion1.wav',
-  'assets/percussion2.wav',
-  'assets/percussion3.wav',
-  'assets/percussion4.wav',
-  'assets/synth.wav',
-  'assets/topsecret.mp3',
+  'src/assets/bass1.wav',
+  'src/assets/bass2.wav',
+  'src/assets/fx1.wav',
+  'src/assets/fx2.wav',
+  'src/assets/melody1.wav',
+  'src/assets/melody2.wav',
+  'src/assets/melody3.wav',
+  'src/assets/melody4.wav',
+  'src/assets/melody5.wav',
+  'src/assets/melody6.wav',
+  'src/assets/percussion1.wav',
+  'src/assets/percussion2.wav',
+  'src/assets/percussion3.wav',
+  'src/assets/percussion4.wav',
+  'src/assets/synth.wav',
+  'src/assets/topsecret.mp3',
 ];
-const useSoundPlayer = () => {
-  const [play] = useSound('', { volume: 1 }); // Initialize with an empty sound
 
-  const playBoard = (soundFile: string) => {
-    play({ sound: soundFile });
-  };
-
-  return { playBoard };
-};
 export const MusicPage = () => {
   const [playIntro] = useSound(intro);
   const keyCount = 16;
-  const keyArray = Array.from({ length: keyCount }, (_, index) => index + 1);
+  const keyArray = Array.from({ length: keyCount }, (_, index) => index);
   useEffect(() => {
     playIntro();
   }, [playIntro]);
-  const { playBoard } = useSoundPlayer();
-  const handleButtonClick = (index: number) => {
-    const soundToPlay = sounds[index];
-    playBoard(soundToPlay);
-  };
 
   return (
     <div className={styles.keyWrapper}>
       <img className={styles.image} src="public/codelex.svg" alt="codelex" />
       <div className={styles.asd}>
         {keyArray.map((index) => (
-          <PlayButton key={index} soundFile={sounds[index]} onClick={() => handleButtonClick(index)}/>
+          <PlayButton key={index} soundFile={sounds[index]}/>
         ))}
       </div>
     </div>
